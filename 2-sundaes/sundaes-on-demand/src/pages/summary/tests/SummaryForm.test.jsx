8b041052db7756e6e19b2feb489392f95a8f5200
Jafeth VV <jafethvv@hotmail.com>
2022-05-01 00:37:07 -0600

    Test and code summary form

---
+++ 2-sundaes/sundaes-on-demand/src/pages/summary/tests/SummaryForm.test.jsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('Initial Conditions', () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole('checkbox', {
+        name: /terms and conditions/i,
+    });
+    expect(checkbox).not.toBeChecked();
+
+    const confirmBtn = screen.getByRole('button', { name: /confirm order/i });
+    expect(confirmBtn).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and disable on second click', () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole('checkbox', {
+        name: /terms and conditions/i,
+    });
+    const confirmBtn = screen.getByRole('button', {
+        name: /confirm order/i,
+    });
+
+    fireEvent.click(checkbox);
+    expect(confirmBtn).toBeEnabled();
+
+    fireEvent.click(checkbox);
+    expect(confirmBtn).toBeDisabled();
+});
