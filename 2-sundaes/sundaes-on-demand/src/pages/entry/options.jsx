be2779101caf8ff7864014e0da8e20ed31c3277d
Jafeth VV <jafethvv@hotmail.com>
2022-05-21 15:08:17 -0600

    add topping option to the test

--- 2-sundaes/sundaes-on-demand/src/pages/entry/options.jsx
+++ 2-sundaes/sundaes-on-demand/src/pages/entry/options.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
-// import ToppingOption from './ToppingOption';
+import ToppingOption from './ToppingOption';
 
 export const Options = ({ optionType }) => {
     const [items, setItems] = useState([]);
@@ -17,7 +17,7 @@ export const Options = ({ optionType }) => {
             });
     }, [optionType]);
 
-    const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+    const ItemComponent = optionType === 'scoops' ? ScoopOption :ToppingOption;
 
     const optionItems = items.map((item) => (
         <ItemComponent
--- 2-sundaes/sundaes-on-demand/src/pages/entry/tests/options.test.jsx
+++ 2-sundaes/sundaes-on-demand/src/pages/entry/tests/options.test.jsx
@@ -12,5 +12,23 @@ test('display img for each scoop option from server', async () => {
 
     // confirm alt text of images
     const altText = scoopImages.map((image) => image.alt);
-    expect(altText).toEqual(['Chocolate scoop', 'Vainilla scoop']);
+    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('Displays image for each toppings option from server', async () => {
+    // Mock Service Worker will return three toppings from server
+    render(<Options optionType="toppings" />);
+
+    // find images, expect 3 based on what msw returns
+    const images = await screen.findAllByRole('img', { name: /topping$/i });
+    expect(images).toHaveLength(3);
+
+    // check the actual alt text for the images
+    // @ts-ignore
+    const imageTitles = images.map((img) => img.alt);
+    expect(imageTitles).toEqual([
+        'Cherries topping',
+        'M&Ms topping',
+        'Hot fudge topping',
+    ]);
 });
